a83db9b9f70323007e95b8b0d76a071f94768632
Gordon Anderson <gordon.b.anderson@gmail.com>
2010-03-24 11:14:16 +0700

    Places: Removed debug

--- lib/fleakr/objects/place.rb
+++ lib/fleakr/objects/place.rb
@@ -41,31 +41,7 @@ module Fleakr
       lazily_load :timezone, :has_shapedata, :shapefile_url, :with => :load_info
       
       scoped_search
-      
-=begin
-<?xml version="1.0" encoding="utf-8" ?>
-<rsp stat="ok">
-<place place_id="05s_Z7KaBJlofQ" woeid="35567" latitude="56.339" longitude="-2.796" 
-place_url="/United+Kingdom/Scotland/St.+Andrews" place_type="locality" place_type_id="7" 
-timezone="Europe/London" name="St. Andrews, Scotland, United Kingdom" has_shapedata="1">
-     <locality place_id="05s_Z7KaBJlofQ" woeid="35567" latitude="56.339" longitude="-2.796" 
-     place_url="/United+Kingdom/Scotland/St.+Andrews">St. Andrews, Scotland, United Kingdom
-     </locality>
-     <county place_id="P8fAbESYA5q7lBUAPw" woeid="12602201" latitude="56.229" longitude="-3.161" place_url="/P8fAbESYA5q7lBUAPw">Fife, Scotland, United Kingdom</county>
-     <region place_id="yw00oOWYA5mnH740VQ" woeid="12578048" latitude="56.652" longitude="-3.996" place_url="/United+Kingdom/Scotland">Scotland, United Kingdom</region>
-     <country place_id="DevLebebApj4RVbtaQ" woeid="23424975" latitude="54.314" longitude="-2.230" place_url="/United+Kingdom">United Kingdom</country>
-     <shapedata created="1247719263" alpha="0.00015" count_points="2116" count_edges="41" has_donuthole="0" is_donuthole="0">
-             <polylines>
-                     <polyline>56.366817474365,-2.819885969162 56.366962432861,-2.819365978241 56.365249633789,-2.8135290145874 56.365440368652,-2.8102679252625 56.361637115479,-2.8058910369873 56.360969543457,-2.8044309616089 56.353839874268,-2.7973930835724 56.345275878906,-2.7912130355835 56.343467712402,-2.7874369621277 56.341567993164,-2.7835750579834 56.342540740967,-2.7716009616852 56.334098815918,-2.758425951004 56.332599639893,-2.7486839294434 56.329578399658,-2.7411739826202 56.325199127197,-2.736711025238 56.324142456055,-2.7269690036774 56.326782226562,-2.7211329936981 56.327613830566,-2.7188580036163 56.328186035156,-2.7084300518036 56.312297821045,-2.7154250144958 56.305755615234,-2.7342491149902 56.302967071533,-2.7567100524902 56.321628570557,-2.7493278980255 56.328147888184,-2.7575249671936 56.326770782471,-2.7675240039825 56.321105957031,-2.7771370410919 56.317867279053,-2.7902688980103 56.320343017578,-2.7973930835724 56.320022583008,-2.8171129226685 56.313159942627,-2.8224630355835 56.313167572021,-2.8224999904633 56.331684112549,-2.8289999961853 56.320350646973,-2.8453669548035 56.331954956055,-2.8516380786896 56.334987640381,-2.8428189754486 56.351791381836,-2.8380770683289 56.36429977417,-2.8399651050568 56.36389541626,-2.834214925766 56.364490509033,-2.8283779621124 56.365737915039,-2.8253529071808 56.366817474365,-2.819885969162</polyline>
-             </polylines>
-             <urls>
-                     <shapefile>http://farm3.static.flickr.com/2653/shapefiles/35567_20090716_d8d512008b.tar.gz</shapefile>
-             </urls>
-     </shapedata>
-</place>
-</rsp>
-=end
-      
+
       #Find by woe id
       find_one :by_woe_id, :using => :woe_id, :call => 'places.getInfo', :path => 'rsp/place'
 
@@ -98,6 +74,7 @@ timezone="Europe/London" name="St. Andrews, Scotland, United Kingdom" has_shaped
         
       end
       
+      
       def locality
         @locality
       end
@@ -115,9 +92,7 @@ timezone="Europe/London" name="St. Andrews, Scotland, United Kingdom" has_shaped
       end
       
       
-      
       def self.find_all_by_tags(tags, place_type_id, woe_id = nil)
-        puts "WOE ID:#{woe_id}"
         if woe_id.blank?
           response = Fleakr::Api::MethodRequest.with_response!('places.placesForTags', :tags => tags, :place_type_id => place_type_id)
         else
@@ -126,9 +101,9 @@ timezone="Europe/London" name="St. Andrews, Scotland, United Kingdom" has_shaped
         (response.body/'places/place').map {|e| Place.new(e) }
       end
       
+      
       #This returns places for the currently authenticated user
       def self.find_all_by_authenticated_user(place_type_id = 8, woe_id = nil)
-        puts "WOE ID:#{woe_id}"
         if woe_id.blank?
           response = Fleakr::Api::MethodRequest.with_response!('places.placesForUser', :place_type_id => place_type_id)
         else
@@ -140,7 +115,6 @@ timezone="Europe/London" name="St. Andrews, Scotland, United Kingdom" has_shaped
 
 
       # A list of related tags.  Each of the objects in the collection is an instance of Tag
-      #
       def tags
         @tags ||= begin
           Tag.find_all_by_woe_id(@woeid)
@@ -149,18 +123,15 @@ timezone="Europe/London" name="St. Andrews, Scotland, United Kingdom" has_shaped
       
       #Overcome issue with name being an attribute or a text node depending on call used
       def initialize(document = nil, options = {})
-        puts "IN PLACE OVERRIDE METHOD"
         self.populate_from(document) unless document.nil?
         @authentication_options = options.extract!(:auth_token)
         
         if @name.blank?
-          puts "BLANK NAME :( *********"
           @name = document.at('.').inner_text
         else
           @brief = false
         end
         
-        puts "BRIEF:#{@brief}"
       end
     end
     
