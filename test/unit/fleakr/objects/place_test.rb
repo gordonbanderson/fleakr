2e64b92513eb8e0471a62a5a771f9b57c11cf010
Gordon Anderson <gordon.b.anderson@gmail.com>
2010-04-12 17:56:03 +0700

    Places: Added test for ensuring values correctly extracted from an XML record

--- test/unit/fleakr/objects/place_test.rb
+++ test/unit/fleakr/objects/place_test.rb
@@ -10,6 +10,30 @@ module Fleakr::Objects
       should_find_one :place, :by => :woe_id, :call => 'places.getInfo', :path => 'rsp/place'
       should_find_one :place, :by => :url, :call => 'places.getInfoByUrl', :path => 'rsp/place'
     end
+    
+    
+    
+    context "An instance of the Place class" do
+      
+      setup { @place = Place.new }
+      
+      context "when populating from the places_getInfo XML data" do
+        setup do
+          @object = Place.new(Hpricot.XML(read_fixture('places.getInfo')).at('rsp/place'))
+        end
+        
+        should_have_a_value_for :latitude   => '50.782'
+        should_have_a_value_for :longitude  => '0.290'
+        should_have_a_value_for :place_id   => 'O8Jf6HCYCJ3dUw'
+        should_have_a_value_for :timezone  => 'Europe/London'
+        should_have_a_value_for :woe_id  => '19135'
+        should_have_a_value_for :place_url  => '/United+Kingdom/England/Eastbourne'
+        should_have_a_value_for :place_type  => 'locality'
+        should_have_a_value_for :place_type_id  => '7'
+        should_have_a_value_for :name  => 'Eastbourne, England, United Kingdom'
+        
+      end
+    end
 
   end
 end
